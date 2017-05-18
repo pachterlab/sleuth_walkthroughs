dfcee86c97a5d268d1ded859c1caaecf7b364416
Harold Pimentel <haroldpimentel@gmail.com>
2017-05-17 23:13:06 -0700

    scaffold bottomly #vc

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from os.path import splitext
+
 ###
 # functions
 ###
@@ -6,6 +8,6 @@ def source_r(base, fname):
 
 def source_rmd(base, file_name, output_name = None):
     if output_name is None:
-        output_name = splitext(file_name)[0]
+        output_name = '../' + splitext(file_name)[0]
         output_name += '.html'
     return 'OMP_NUM_THREADS=1 Rscript --vanilla --default-packages=methods,stats,utils,knitr -e \'setwd("{0}")\' -e \'rmarkdown::render("{1}", output_file = "{2}")\''.format(base, file_name, output_name)
